ad6bf095ee7e3ab20a20882f336a1eed2d70e6b1
yuhexiong <yuhexiong871210@gmail.com>
2024-02-01 17:52:11 +0800

    feat: task schema

--- model/permission.go
+++ model/permission.go
@@ -9,6 +9,6 @@ const PermissionCollName = "permission"
 type Permission struct {
 	ID       *primitive.ObjectID `bson:"_id,omitempty" json:"_id" example:"623853b9503ce2ecdd221c94"`
 	BaseData `bson:"inline"`
-	Category string `bson:"category,omitempty" json:"category" example:"USER"` // 權限種類
-	Code     string `bson:"code,omitempty" json:"code" example:"createUser"`   // 權限代號
+	Category string `bson:"category" json:"category" example:"USER"` // 權限種類
+	Code     string `bson:"code" json:"code" example:"createUser"`   // 權限代號
 }
