93fc61eb34f44c4a1f4495824c439f682e993977
yuhexiong <yuhexiong871210@gmail.com>
2024-03-24 14:14:05 +0800

    feat: create notification function

---
+++ controller/notification.go
@@ -0,0 +1,21 @@
+package controller
+
+import (
+	"permission-api/model"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type CreateNotificationOpts struct {
+	ToUserOId *primitive.ObjectID `bson:"ToUserOId" json:"ToUserOId" binding:"required" example:"abd1234"` // 接收者帳號id
+	Content   string              `bson:"content" json:"content" binding:"required" example:"您有一個新的任務"`    // 通知內容
+}
+
+// 建立通知
+func CreateNotification(opts CreateNotificationOpts, result *model.Notification) error {
+	createdNotification := &model.Notification{
+		ToUserOId: opts.ToUserOId,
+		Content:   opts.Content,
+	}
+	return model.Insert(model.NotificationCollName, createdNotification, result)
+}
