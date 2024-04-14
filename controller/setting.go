fbb7eedc58ac8018355668d2a2986b9140cd9812
yuhexiong <yuhexiong871210@gmail.com>
2024-04-14 19:34:10 +0800

    feat: update setting value controller

--- controller/setting.go
+++ controller/setting.go
@@ -4,9 +4,15 @@ import (
 	"permission-api/model"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 // 依code取得設定
-func GetSettingByCode(code *string, result *model.User) error {
+func GetSettingByCode(code *string, result *model.Setting) error {
 	return model.GetByFilter(model.SettingCollName, bson.D{{Key: "code", Value: *code}}, &result)
+}
+
+// 更新設定值
+func UpdateSettingValue(objectId *primitive.ObjectID, value *string) error {
+	return model.Update(model.SettingCollName, objectId, bson.D{{Key: "value", Value: *value}})
 }
